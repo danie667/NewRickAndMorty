3351d1c4cc63ba19f8878d99e1c2eca2dd36a7b1
danie667 <bombuuk@gmail.com>
2023-05-13 15:13:37 +0600

    Created Rick and Morty project

--- againNewProjectMorty/controllers/core/RMTabViewController.swift
+++ againNewProjectMorty/controllers/core/RMTabViewController.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 
-class RMTabViewController: UIViewController {
+final class RMTabViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpTabs()
+    }
+    
+    private func setUpTabs () {
+        let characterVC = RMCharacterViewController()
+        let settingsVC = RMSettingsViewController()
+        let locationVC = RMLocationViewController()
+        let episodeVC = RMEpisodeViewController()
+    
+        
+        let vc1 = UINavigationController(rootViewController: characterVC)
+        let vc2 = UINavigationController(rootViewController: settingsVC)
+        let vc3 = UINavigationController(rootViewController: locationVC)
+        let vc4 = UINavigationController(rootViewController: episodeVC)
+        
+        let massive = [vc1, vc2, vc3, vc4]
+        for i in massive {
+            i.navigationBar.prefersLargeTitles = true
+        }
+        
+        
+        
+        vc1.tabBarItem = UITabBarItem(title: "Character",
+                                      image: UIImage(systemName: "person"),
+                                      tag: 1)
+        vc2.tabBarItem = UITabBarItem(title: "Settings",
+                                      image: UIImage(systemName: "shield.righthalf.filled"),
+                                      tag: 2)
+        vc3.tabBarItem = UITabBarItem(title: "Location",
+                                      image: UIImage(systemName: "network"),
+                                      tag: 3)
+        vc4.tabBarItem = UITabBarItem(title: "Episode",
+                                      image: UIImage(systemName: "photo.fill"),
+                                      tag: 4)
+        
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
 
