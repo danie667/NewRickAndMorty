5f34a4a8ee73bb127aa6cfb981eec677f3cb5c3f
danie667 <bombuuk@gmail.com>
2023-05-13 14:34:29 +0600

    Initial Commit

---
+++ againNewProjectMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  againNewProjectMorty
+//
+//  Created by Daniyal Abuov on 13.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
