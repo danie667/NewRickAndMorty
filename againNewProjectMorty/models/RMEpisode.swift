2e85e7a32b0e76ae8f79f48b75b56e92eaf8a762
danie667 <bombuuk@gmail.com>
2023-05-13 14:55:51 +0600

    first commit

---
+++ againNewProjectMorty/models/RMEpisode.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  againNewProjectMorty
+//
+//  Created by Daniyal Abuov on 13.05.2023.
+//
+
+import Foundation
---
+++ againNewProjectMorty/models/RMLocation.swift
@@ -0,0 +1,8 @@
+//
+//  RMLocation.swift
+//  againNewProjectMorty
+//
+//  Created by Daniyal Abuov on 13.05.2023.
+//
+
+import Foundation
